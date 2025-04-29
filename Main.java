1fd34f4153887a68466f81e4fc1542aa2f8b30a6
SpacEagle17 <46494907+SpacEagle17@users.noreply.github.com>
2025-04-29 17:26:08 +0200

    1.6.0

--- Main.java
+++ Main.java
@@ -12,8 +12,8 @@ import java.util.List;
 public class Main {
     // Configurable constants
     public static final String PATCH_NAME = "EuphoriaPatches";
-    public static final String VERSION = "_r5.4";
-    public static final String PATCH_VERSION = "_1.5.1";
+    public static final String VERSION = "_r5.5";
+    public static final String PATCH_VERSION = "_1.6.0";
     public static final List<String> STYLES = Arrays.asList("Reimagined", "Unbound");
 
     // Encryption constants
